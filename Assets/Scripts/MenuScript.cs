f5113e49018d748c042e8a0e7f5aea9f1d0892fb
kesh1092 <qtip1092@gmail.com>
2017-10-07 19:59:55 -0600

    Original Files

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Title screen script
+/// </summary>
+public class MenuScript : MonoBehaviour
+{
+//	public int incTest = 0; //doesnt increment
+	public void StartGame()
+	{
+//		incTest++;
+		// "Stage1" is the name of the first scene we created.
+		//originally:         Application.LoadLevel("Stage1");
+		SceneManager.LoadScene("Stage1");
+	}
+}
+
---
+++ Assets/Scripts/ShotScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ShotScript : MonoBehaviour {
+
+	public int damage = 1;
+	public bool isEnemyShot;// set to true in shot prefab
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, 4);
+	}
+	
+ 
+}

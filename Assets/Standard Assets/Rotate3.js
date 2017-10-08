46b2e27126c435eae2436117f4ca5314c391033f
kesh1092 <qtip1092@gmail.com>
2017-10-07 19:57:53 -0600

    no message

--- Assets/Standard Assets/Rotate3.js
+++
@@ -1,9 +0,0 @@
-﻿// #pragma strict
-
- private var speed = 2.0/4;
- 
- function Update() {
-     var angle = (Mathf.Sin(Time.time * speed) + 1.0) / 2.0 * 360.0;
-     transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
- 
- }

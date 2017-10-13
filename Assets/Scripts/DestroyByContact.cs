70bd75b3813fc84c6c16f94daee41ca3c501b3c9
Zachary Morgan <zacp635@nmsu.edu>
2017-10-12 21:06:43 -0600

    Made player die if they touch the ball, and made a prompt telling player to press R after game over to restart game!

---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyByContact : MonoBehaviour 
+{ //Order is ignored for destroy, as the object isnt immedietly destroyed but instead put in a queue to be destroyed at the end of each frame.
+
+	public GameObject explosion;
+	public GameObject playerExplosion;
+	//public int scoreValue;
+	private GameController gameController;
+	public GameObject text;
+
+	void Start()
+	{
+		GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+		if(gameControllerObject != null)
+		{
+			gameController = gameControllerObject.GetComponent <GameController>();
+		}
+		if (gameController == null) 
+		{
+			Debug.Log("Cannot find 'GameController' script");
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		//Debug.Log (other.name);
+		//if (other.tag == "Boundary") 
+		//{
+		//	return;
+		//}
+		//Instantiate(explosion, transform.position, transform.rotation);
+		if (other.tag == "Player") 
+		{
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+			gameController.GameOver();
+			Destroy(other.gameObject);
+		}
+		//gameController.AddScore (scoreValue);
+		//Destroy(other.gameObject);
+		//Destroy(gameObject);
+	}
+}

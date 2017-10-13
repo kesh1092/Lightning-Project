2ee8143c24ac0c84495d54f8cad7f18066971101
Jonathan Frank Steele <jonathan@steeleconsult.com>
2017-10-12 21:24:02 -0600

    Fixed he Rotating Square functionality and a broken error

---
+++ Assets/Scripts/Rotate_Square.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Square : MonoBehaviour {
+	public float speed = 66F;
+	void Update() {
+
+		if (Input.GetKey(KeyCode.LeftBracket))
+			transform.Rotate (new Vector3 (0, 0, 1) * Time.deltaTime * speed); 
+		if (Input.GetKey(KeyCode.RightBracket))
+			transform.Rotate (new Vector3 (0, 0, -(1)) * Time.deltaTime * speed); 
+	}
+}
